4a241bc2caee917c2550e00a585395bf84352f57
avreenco <h.asgharnejad@avreenco.com>
2022-04-06 15:00:27 +0430

    remove unuseful code's

--- avreen-iso8583/src/main/java/io/avreen/iso8583/util/IErrorCode.java
+++ avreen-iso8583/src/main/java/io/avreen/iso8583/util/IErrorCode.java
@@ -9,21 +9,8 @@ public interface IErrorCode {
      */
     Integer MandatoryFieldNotExist = 1001;
     /**
-     * The constant InavlidFieldLen.
-     */
-    Integer InvalidFieldLen = 1002;
-    /**
      * The constant InavlidFieldFormat.
      */
     Integer InvalidFieldFormat = 1003;
-    /**
-     * The constant InvalidAmountField.
-     */
-    Integer InvalidAmountField = 1004;
-
-    /**
-     * The constant InvalidAccountFormat.
-     */
-    Integer InvalidAccountFormat = 1006;
 
 }
